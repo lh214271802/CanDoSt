b51585c5cedbbae11ca7ba2a9b3598619009c059
lh214271802 <214271802@qq.com>
2017-11-21 16:51:25 +0800

    MY FUNNY FRAME

--- app/src/main/java/cn/lh/candost/MainActivity.java
+++ app/src/main/java/cn/lh/candost/MainActivity.java
@@ -8,7 +8,6 @@ import com.anbetter.danmuku.DanMuParentView;
 import com.anbetter.danmuku.DanMuView;
 import com.anbetter.danmuku.model.DanMuModel;
 import com.anbetter.danmuku.model.utils.DimensionUtil;
-import com.anbetter.danmuku.view.OnDanMuTouchCallBackListener;
 import com.blankj.utilcode.util.ToastUtils;
 import com.bumptech.glide.Glide;
 import com.lh.base.activity.BaseRefreshActivity;
@@ -20,7 +19,6 @@ import com.lh.util.StatusBarUtil;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import cn.jzvd.JZVideoPlayer;
 import cn.jzvd.JZVideoPlayerStandard;
@@ -50,96 +48,56 @@ public class MainActivity extends BaseRefreshActivity {
 //        BarUtils.setStatusBarAlpha(this);
         StatusBarUtil.StatusBarLightMode(this);
         initRefreshLayout(true, false);
-        findViewById(R.id.select_image).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SelectImageActivity.show(mContext,
-                        new SelectOptions.Builder()
-                                .setHasCam(true)
-                                .setSelectCount(1)
-                                .setCrop(720, 720)
-                                .setCallback(new SelectOptions.Callback() {
-                                    @Override
-                                    public void doSelected(List<String> videos) {
-                                        Glide.with(mContext).load(videos.get(0)).into(imageView);
-                                    }
-                                })
-                                .build());
-
-            }
-        });
-        findViewById(R.id.image_gallery).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ImageGalleryActivity.startActivity(mContext, 0, new ArrayList<String>() {{
-                    add("http://img06.tooopen.com/images/20161029/tooopen_sy_183839859946.jpg");
-                    add("http://img.zcool.cn/community/010fc7554b7d28000001bf721916fc.jpg@1280w_1l_2o_100sh.jpg");
-                    add("http://f.hiphotos.baidu.com/image/pic/item/3ac79f3df8dcd1008742b1cc788b4710b8122f04.jpg");
-                    add("http://h.hiphotos.baidu.com/image/pic/item/f31fbe096b63f624bcc66aea8d44ebf81b4ca344.jpg");
-                    add("http://img0.imgtn.bdimg.com/it/u=2020395685,1666600559&fm=214&gp=0.jpg");
-                    add("http://ww2.sinaimg.cn/large/85d77acdgw1f4hs72wzxkg20dw07t7k3.jpg");
-                    add("http://ww1.sinaimg.cn/large/85cccab3gw1etdl4dxvt0g20ci071kjl.jpg");
-                }}, true);
-            }
-        });
-        findViewById(R.id.select_video).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SelectVideoActivity.startActivity(mContext, new SelectOptions.Builder()
-                        .setCallback(new SelectOptions.Callback() {
-                            @Override
-                            public void doSelected(List<String> images) {
-                                videoPlayer.setUp(images.get(0), JZVideoPlayer.SCREEN_WINDOW_NORMAL, "嫂子请闭眼");
-                                Glide.with(mContext).load(images.get(0)).into(videoPlayer.thumbImageView);
-                            }
-                        })
+        findViewById(R.id.select_image).setOnClickListener(v -> SelectImageActivity.show(mContext,
+                new SelectOptions.Builder()
                         .setHasCam(true)
-                        .setSelectCount(5).build());
-            }
-        });
-        findViewById(R.id.go_taoke).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TaoKeActivity.startActivity(mContext);
-            }
-        });
-        findViewById(R.id.go_weixin_best).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                WeiXinBestActivity.startActivity(mContext);
-            }
-        });
-        findViewById(R.id.danmu_show).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DanMuModel danMuModel = new DanMuModel();
-                danMuModel.setDisplayType(DanMuModel.RIGHT_TO_LEFT);
-                danMuModel.setPriority(DanMuModel.NORMAL);
-                danMuModel.marginLeft = DimensionUtil.dpToPx(mContext, 30);
+                        .setSelectCount(1)
+                        .setCrop(720, 720)
+                        .setCallback(videos -> Glide.with(mContext).load(videos.get(0)).into(imageView))
+                        .build()));
+        findViewById(R.id.image_gallery).setOnClickListener(v -> ImageGalleryActivity.startActivity(mContext, 0, new ArrayList<String>() {{
+            add("http://img06.tooopen.com/images/20161029/tooopen_sy_183839859946.jpg");
+            add("http://img.zcool.cn/community/010fc7554b7d28000001bf721916fc.jpg@1280w_1l_2o_100sh.jpg");
+            add("http://f.hiphotos.baidu.com/image/pic/item/3ac79f3df8dcd1008742b1cc788b4710b8122f04.jpg");
+            add("http://h.hiphotos.baidu.com/image/pic/item/f31fbe096b63f624bcc66aea8d44ebf81b4ca344.jpg");
+            add("http://img0.imgtn.bdimg.com/it/u=2020395685,1666600559&fm=214&gp=0.jpg");
+            add("http://ww2.sinaimg.cn/large/85d77acdgw1f4hs72wzxkg20dw07t7k3.jpg");
+            add("http://ww1.sinaimg.cn/large/85cccab3gw1etdl4dxvt0g20ci071kjl.jpg");
+        }}, true));
+        findViewById(R.id.select_video).setOnClickListener(view -> SelectVideoActivity.startActivity(mContext, new SelectOptions.Builder()
+                .setCallback(images -> {
+                    videoPlayer.setUp(images.get(0), JZVideoPlayer.SCREEN_WINDOW_NORMAL, "嫂子请闭眼");
+                    Glide.with(mContext).load(images.get(0)).into(videoPlayer.thumbImageView);
+                })
+                .setHasCam(true)
+                .setSelectCount(5).build()));
+        findViewById(R.id.go_taoke).setOnClickListener(view ->
+                TaoKeActivity.startActivity(mContext)
+        );
+        findViewById(R.id.go_weixin_best).setOnClickListener(view -> WeiXinBestActivity.startActivity(mContext));
+        findViewById(R.id.danmu_show).setOnClickListener(view -> {
+            DanMuModel danMuModel = new DanMuModel();
+            danMuModel.setDisplayType(DanMuModel.RIGHT_TO_LEFT);
+            danMuModel.setPriority(DanMuModel.NORMAL);
+            danMuModel.marginLeft = DimensionUtil.dpToPx(mContext, 30);
 
-                // 显示的文本内容
-                danMuModel.textSize = DimensionUtil.spToPx(mContext, 14);
-                danMuModel.textColor = ContextCompat.getColor(mContext, R.color.colorAccent);
-                danMuModel.textMarginLeft = DimensionUtil.dpToPx(mContext, 5);
-                danMuModel.text = "春风油绿江南岸";
+            // 显示的文本内容
+            danMuModel.textSize = DimensionUtil.spToPx(mContext, 14);
+            danMuModel.textColor = ContextCompat.getColor(mContext, R.color.colorAccent);
+            danMuModel.textMarginLeft = DimensionUtil.dpToPx(mContext, 5);
+            danMuModel.text = "春风油绿江南岸";
 
-                // 弹幕文本背景
-                danMuModel.textBackground = ContextCompat.getDrawable(mContext, R.drawable.round_rect_blue);
-                danMuModel.textBackgroundMarginLeft = DimensionUtil.dpToPx(mContext, 15);
-                danMuModel.textBackgroundPaddingTop = DimensionUtil.dpToPx(mContext, 3);
-                danMuModel.textBackgroundPaddingBottom = DimensionUtil.dpToPx(mContext, 3);
-                danMuModel.textBackgroundPaddingRight = DimensionUtil.dpToPx(mContext, 15);
-                danMuModel.enableTouch(true);
-                //弹幕点击事件
-                danMuModel.setOnTouchCallBackListener(new OnDanMuTouchCallBackListener() {
-                    @Override
-                    public void callBack(DanMuModel danMuView) {
-                        ToastUtils.showShort("点击啊");
-                    }
-                });
+            // 弹幕文本背景
+            danMuModel.textBackground = ContextCompat.getDrawable(mContext, R.drawable.round_rect_blue);
+            danMuModel.textBackgroundMarginLeft = DimensionUtil.dpToPx(mContext, 15);
+            danMuModel.textBackgroundPaddingTop = DimensionUtil.dpToPx(mContext, 3);
+            danMuModel.textBackgroundPaddingBottom = DimensionUtil.dpToPx(mContext, 3);
+            danMuModel.textBackgroundPaddingRight = DimensionUtil.dpToPx(mContext, 15);
+            danMuModel.enableTouch(true);
+            //弹幕点击事件
+            danMuModel.setOnTouchCallBackListener(danMuView -> ToastUtils.showShort("点击啊"));
 
-                danmuView.add(danMuModel);
-            }
+            danmuView.add(danMuModel);
         });
         imageView = findViewById(R.id.image_view);
         videoPlayer = findViewById(R.id.video_player);
